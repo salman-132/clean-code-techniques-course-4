7acc73b8a25ee883f017ddbbb4ad211c17656a60
salman-132 <saldaw2004@gmail.com>
2026-07-04 05:22:13 +0300

    Add main.cpp to print a name to the console
    Add solution for problem 01

---
+++ 01-name-in-screen/main.cpp
@@ -0,0 +1,17 @@
+ 
+#include <iostream>
+
+using namespace std;
+
+
+
+void PrintName(string Name ) {
+    cout << "\n your name is Name:" <<Name<< endl;
+}
+int main()
+{
+     
+    PrintName("salman Mahmood");
+}
+
+ 
