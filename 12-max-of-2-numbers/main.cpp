eddf874436d3b98fc92e379751476613c09df7b7
salman-132 <saldaw2004@gmail.com>
2026-07-07 02:02:56 +0300

    Add solution for problem 12

---
+++ 12-max-of-2-numbers/main.cpp
@@ -0,0 +1,29 @@
+ 
+#include <iostream>
+using namespace std;
+
+void ReadNumbers (int& num1,int& num2) {
+	cout << "enter number1:" << endl;
+	cin >> num1;
+
+	cout << "enter number2:" << endl;
+	cin >> num2;
+}
+int MaxNumbers(int num1,int num2) {
+	if (num1 > num2)
+		return num1;
+	else
+		return num2;
+}
+void PrintMaxNumber(int Max ){
+
+	cout << "\n The max number is " << Max;
+}
+
+
+
+int main()
+{
+	int num1, num2;
+	ReadNumbers(num1,num2);
+	PrintMaxNumber(MaxNumbers(num1,num2));
