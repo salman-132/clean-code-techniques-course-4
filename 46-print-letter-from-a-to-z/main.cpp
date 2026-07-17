f69055f9b64436213bdb957594f3d29ee5775522
salman-132 <saldaw2004@gmail.com>
2026-07-17 03:14:02 +0300

    Add solution for problem 46

---
+++ 46-print-letter-from-a-to-z/main.cpp
@@ -0,0 +1,26 @@
+ 
+#include <iostream>
+using namespace std;
+
+
+void printAToZ() {
+
+    for (int i = 65; i <= 90;i++) {
+    
+        cout << char(i)<<endl;
+    
+    }
+
+
+}
+
+
+
+
+int main()
+{
+    printAToZ();
+
+    return 0;
+}
+ 
